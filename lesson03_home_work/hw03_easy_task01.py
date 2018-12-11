53c09cae567fa491bc7f2e6ce1c593725d2e7ace
Kozhanov Alexander <alexander.kozhanov@megafon.ru>
2018-12-11 16:55:16 +0300

    Изменение кода Lesson03 Задача-1 easy

--- lesson03_home_work/hw03_easy_task01.py
+++ lesson03_home_work/hw03_easy_task01.py
@@ -7,9 +7,13 @@
 # Для решения задачи не используйте встроенные функции и функции из модуля math.
 
 def my_round(number, ndigits):
-    return round(number, ndigits)
+    # return round(number, ndigits)
+    number = number * (10 ** ndigits) + 0.41
+    number = number // 1
+    return number / (10 ** ndigits)
 
 
 print(my_round(2.1234567, 5))
 print(my_round(2.1999967, 5))
 print(my_round(2.9999967, 5))
+
